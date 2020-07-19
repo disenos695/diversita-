b0840a6f9e88d161239c6ae58e477575de302272
disenos695 <me4056377@gmail.com>
2020-07-18 20:09:22 -0500

    f inicial

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:kdfW7qKknMh8tHprmsQ1TGJNyj94tv1fH6HeuHCWjUQ='),
 
     'cipher' => 'AES-256-CBC',
 
